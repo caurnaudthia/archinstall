e904f566f14f0525ba4d65455d4f4636daa6b386
wisteria@caliburn <caurnaudthia@gmail.com>
2025-12-28 10:57:57 -0600

    sync

--- instructions.sh
+++ instructions.sh
@@ -106,7 +106,10 @@ sudo pacman -S hugin
 sudo pacman -S perl-image-exiftool
 sudo pacman -S libreoffice
 sudo pacman -S pdfarranger
+paru -S discord-canary
+# sdv things
 sudo pacman -S openssl-1.1
+sudo pacman -S execstack
 
 paru -S anki
 # ensure the following autostart:
